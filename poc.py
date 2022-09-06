42b2ba1c9f2936e5104cb4afe8b6d1e28e459b9b
Steve G <9045562+eurogig@users.noreply.github.com>
2022-09-06 15:15:00 +0100

    Update poc.py

--- poc.py
+++ poc.py
@@ -108,7 +108,7 @@ def main() -> None:
     init(autoreset=True)
     print(Fore.BLUE + """
 [!] CVE: CVE-2021-44228
-[!] Github repo: https://github.com/kozmer/log4j-shell-poc
+[!] Github repo: https://github.com/eurogig/log4sheller
 """)
 
     parser = argparse.ArgumentParser(description='log4shell PoC')
